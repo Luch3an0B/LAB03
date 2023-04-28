5aec3d2c6a6db37f32870fdff795be509101aaa1
luciano <luciano.bortolameoll@usm.cl>
2023-04-28 11:59:08 -0400

    ver como arreglar y funcionamiento de vistas

--- INFJMC/core/views.py
+++ INFJMC/core/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Hola Mundo")
+
+def home(request):
+    return render(request,'core/home.html')
+
+def carreras(request):
+    return HttpResponse()
+
+def docentes(request):
+    return HttpResponse()
